f60c30c6b048a4923a9960ca1c060b63dace85ec
cepthomas <cepthomas@gmail.com>
2023-11-19 16:52:15 -0600

    WIP.

--- Test/run_test.cmd
+++ Test/run_test.cmd
@@ -4,11 +4,9 @@ echo off
 :: Script to run LuaBagOfTricks unit tests.
 
 :: Set the lua path.
-set LUA_PATH=;;^
-C:\Dev\repos\Lua\LuaBagOfTricks\?.lua;^
-C:\Dev\repos\Lua\LuaBagOfTricks\Test\?.lua;
+set LUA_PATH=;;C:\Dev\repos\Lua\LuaBagOfTricks\?.lua;C:\Dev\repos\Lua\LuaBagOfTricks\Test\?.lua;
 
-:: Run the unit tests. test_pnut test_utils test_class
+:: Run the unit tests.
 pushd ".."
-lua pnut_runner.lua Test\test_class
+lua pnut_runner.lua Test\test_utils.lua Test\test_class.lua Test\test_pnut.lua
 popd
