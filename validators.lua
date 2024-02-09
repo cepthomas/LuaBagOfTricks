8f6b606f951f8a46112ea5a798a5289b8941187c
cepthomas <cepthomas@gmail.com>
2024-02-09 16:59:20 -0600

    WIP.

--- validators.lua
+++ validators.lua
@@ -6,15 +6,6 @@ local M = {}
 
 
 -----------------------------------------------------------------------------
--- TODO2 better way to do this?
-M.fatal = false
-M.depth = 2
-function M.set_mode(fatal, depth)
-    M.fatal = fatal 
-    M.depth = depth
-end
-
------------------------------------------------------------------------------
 function M.is_integer(v) return v ~= nil and math.ceil(v) == v end
 function M.is_number(v) return v ~= nil and type(v) == 'number' end
 function M.is_string(v) return v ~= nil and type(v) == 'string' end
@@ -23,68 +14,61 @@ function M.is_function(v) return v ~= nil and type(v) == 'function' end
 function M.is_table(v) return v ~= nil and type(v) == 'table' end
 
 -----------------------------------------------------------------------------
-function M.val_number(v, min, max, info)
+function M.val_number(v, min, max, name)
     local ok = M.is_number(v)
     ok = ok and (max ~= nil and v <= max)
     ok = ok and (min ~= nil and v >= min)
-    if not ok and M.fatal then
-        local s = string.format("Invalid number:%s %s %s", tostring(v), info or "")
-        error(s, M.depth) -- who called me
+    if not ok then
+        return string.format("Invalid number %s: %s", name, tostring(v))
     end
-    return ok
+    return nil
 end
 
 -----------------------------------------------------------------------------
-function M.val_integer(v, min, max, info)
+function M.val_integer(v, min, max, name)
     local ok = M.is_integer(v)
     ok = ok and (max ~= nil and v <= max)
     ok = ok and (min ~= nil and v >= min)
-    if not ok and M.fatal then
-        local s = string.format("Invalid integer:%s %s", tostring(v), info or "")
-        return s
-        -- error(s, M.depth) -- who called me
+    if not ok then
+        return string.format("Invalid integer %s: %s", name, tostring(v))
     end
-    return ok
+    return nil
 end
 
 -----------------------------------------------------------------------------
-function M.val_string(v, info)
+function M.val_string(v, name)
     local ok = M.is_string(v)
-    if not ok and M.fatal then
-        local s = string.format("Invalid string:%s %s", tostring(v), info or "")
-        error(s, M.depth) -- who called me
+    if not ok then
+        return string.format("Invalid string %s: %s", name, tostring(v))
     end
-    return ok
+    return nil
 end
 
 -----------------------------------------------------------------------------
-function M.val_boolean(v, info)
+function M.val_boolean(v, name)
     local ok = M.is_boolean(v)
-    if not ok and M.fatal then
-        local s = string.format("Invalid boolean:%s %s", tostring(v), info or "")
-        error(s, M.depth) -- who called me
+    if not ok then
+        return string.format("Invalid boolean %s: %s", name, tostring(v))
     end
-    return ok
+    return nil
 end
 
 -----------------------------------------------------------------------------
-function M.val_table(v, info)
+function M.val_table(v, name)
     local ok = M.is_table(v)
-    if not ok and M.fatal then
-        local s = string.format("Invalid table:%s", info or "")
-        error(s, M.depth) -- who called me
+    if not ok then
+        return string.format("Invalid table %s", name)
     end
-    return ok
+    return nil
 end
 
 -----------------------------------------------------------------------------
-function M.val_function(v, info)
+function M.val_function(v, name)
     local ok = M.is_function(v)
-    if not ok and M.fatal then
-        local s = string.format("Invalid function:%s", info or "")
-        error(s, M.depth) -- who called me
+    if not ok then
+        return string.format("Invalid function %s", name)
     end
-    return ok
+    return nil
 end
 
 -----------------------------------------------------------------------------
