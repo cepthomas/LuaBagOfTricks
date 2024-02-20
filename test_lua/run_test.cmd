0e41b296a4648aca4b13984cd832a2e8ea09bef7
cepthomas <cepthomas@gmail.com>
2024-02-20 16:31:49 -0600

    Test reorg.

--- test_lua/run_test.cmd
+++ test_lua/run_test.cmd
@@ -4,10 +4,10 @@ echo off
 :: Set the lua path.
 set LUA_PATH=;;C:\Dev\repos\Lua\LuaBagOfTricks\?.lua;C:\Dev\repos\Lua\LuaBagOfTricks\Test\?.lua;
 
-pushd "..\.."
+pushd ".."
 
 :: Run the unit tests.
 rem  test_stringex.lua  test_utils.lua  test_class.lua  test_pnut.lua
-lua pnut_runner.lua  test_code\test_code_lua\test_pnut.lua
+lua pnut_runner.lua  test_lua\test_pnut.lua
 
 popd
