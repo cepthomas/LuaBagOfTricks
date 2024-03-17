f435c6232cbb958ea8f2528178cf49ff6246ef86
cepthomas <cepthomas@gmail.com>
2024-03-17 17:26:26 -0500

    WIP.

--- test_ch/gen_interop.cmd
+++ test_ch/gen_interop.cmd
@@ -5,5 +5,5 @@ echo off
 
 :: Build the interop. Note: need explicit paths - lua doesn't know file system.
 pushd ".."
-lua gen_interop.lua -ch -d test_ch\interop_spec_ch.lua test_ch
+lua gen_interop.lua -ch test_ch\interop_spec_ch.lua test_ch
 popd
--- test_cs/gen_interop.cmd
+++ test_cs/gen_interop.cmd
@@ -5,5 +5,5 @@ echo off
 
 :: Build the interop. Note: need explicit paths - lua doesn't know file system.
 pushd ".."
-lua gen_interop.lua -cs -d test_cs\interop_spec_cs.lua test_cs
+lua gen_interop.lua -cs test_cs\interop_spec_cs.lua test_cs
 popd
