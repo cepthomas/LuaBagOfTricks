78d4109ea0e0d27854ca466082042576c020641a
cepthomas <cepthomas@gmail.com>
2025-04-24 10:51:59 -0500

    WIP.

--- dev.lua
+++ dev.lua
@@ -1,45 +1,179 @@
+-------------------- A sandbox ------------------------
+
 ---@diagnostic disable: unused-function, unused-local
 local ut = require('lbot_utils')
 local sx = require('stringex')
+local tx = require("tableex")
+require('List')
 
 
--- TODOL >>> https://www.sublimetext.com/docs/build_systems.html#advanced-example
+
+---------------------------------------------------------------------------
+--[[  Simple objects.
+! Need to store private data not exposed by the main table.
+
+- https://www.lua.org/pil/16.1.html
+- https://www.reddit.com/r/lua/comments/tia21g/comment/i1dok2a/
+- http://lua-users.org/wiki/SimpleLuaClasses
+]]
+
+-- Using shared metatable and klunky '_P' table holding private fields. 288 bytes per instance.
+Account1 = {}
+Account1.__index = Account1
+Account1.__tostring = function(t) return string.format('[%s:%s:%d]', t:class(), t:name(), t.balance) end
+Account1.__newindex = function(t, index, value) rawset(t, index, value) end
+-- Account1.__newindex = function(t, index, value) error('__newindex not supported') end
+
+function Account1:create(name, balance)
+    local acc =
+    {
+        _P =
+        {
+            class = 'Account1',
+            name = name or 'no_name',
+            -- __index = Account1,
+            -- __metatable = Account1
+        }
+    }
+    setmetatable(acc, Account1)
+
+    -- public data
+    acc.balance = balance
+    return acc
+end
+
+function Account1:name() return self._P.name end
+function Account1:class() return self._P.class end
+function Account1:withdraw(amount) self.balance = self.balance - amount end
+function Account1:getbalance() return self.balance end
 
 
--------------------- A sandbox ------------------------
+-- Using individual metatables holding private fields. 439 bytes per per instance.
+Account2 = {}
 
---[[
-?? organize modules/globals: https://www.lua.org/pil/15.4.html
-pl import/require
-require 'pl' -- calls Penlight\lua\pl\init.lua
-utils.import 'pl.func' -- take a table/module and 'inject' it into the local namespace.
-local ops = require 'pl.operator' -- normal import
-local List = require 'pl.List' -- class
-local append, concat = table.insert, table.concat -- aliases
-local optable = ops.optable -- alias
-?? add init.lua file? see http://www.playwithlua.com/?p=64
--- check the name before the assignment.
-function M.check_open_package(name) -->> check_global_name?
- for n, v in pairs(name) do
-     if _G[n] ~= nil then
-         error("name clash: " .. n .. " is already defined")
-     end
-     _G[n] = v
- end
+function Account2:create(name, balance)
+    local acc = {}
+    local mt =
+    {
+        -- private info
+        class = 'Account2',
+        name = name or 'no_name',
+        -- class functions
+        __index = Account2,
+        __tostring = function(t) return string.format('[%s:%s:%d]', getmetatable(t).class, getmetatable(t).name, t.balance) end,
+        __newindex = function(t, index, value) rawset(t, index, value) end
+    }
+    setmetatable(acc, mt)
+
+    -- public data
+    acc.balance = balance
+    return acc
+end
+
+function Account2:name() return getmetatable(self).name end
+function Account2:class() return getmetatable(self).class end
+function Account2:withdraw(amount) self.balance = self.balance - amount end
+function Account2:getbalance() return self.balance end
+
+
+-- Using closures + some meta, XXX bytes per per instance.
+function Account3(name, balance)
+    local acc = {}
+
+    function acc:name() return getmetatable(self).name end
+    function acc:class() return getmetatable(self).class end
+    function acc:withdraw(amount) self.balance = self.balance - amount end
+    function acc:getbalance() return self.balance end
+
+    -- Object has been created. Finish up data initialization.
+    setmetatable(acc,
+    {
+        class = 'Account3',
+        name = name or 'no_name',
+        __tostring = function(t) return string.format('[%s:%s:%d]', getmetatable(t).class, getmetatable(t).name, t.balance) end,
+    })
+
+    -- public data
+    acc.balance = balance
+    return acc
+end
+
+local function test_simple_objects()
+
+    local acc = Account1
+
+    -- client create and use an Account
+    local acc_bob = acc:create('bob_name', 1000)
+    print('10', 'bob got:', acc_bob:getbalance())
+    acc_bob:withdraw(100)
+    print('15', 'bob got:', acc_bob:getbalance())
+
+    acc_bob['added'] = 1234
+
+    print('20', 'acc_bob:', acc_bob)
+
+    -- print('25', 'acc_bob:', acc_bob, tx.dump_table(acc_bob, 0, 'acc_bob table'))
+    print('30', acc_bob:name(), acc_bob, tx.dump_table(acc_bob, 0, acc_bob:name()))
+
+    -- print('I got:', acc_bob:getbalance())
+
+    local acc_mary = acc:create('mary_name', 1234)
+    print('35', 'mary got:', acc_mary.balance)
+    print('40', 'mary got:', acc_mary:getbalance())
+
+    print('45', 'acc_mary:', acc_mary)
+    print('50', 'acc_bob:', acc_bob)
+
+
+    --  Measure object sizes.
+    local start_size = collectgarbage('count')
+    print('start', start_size)
+    local store = {}
+    local num = 10000
+
+    for i = 1, num do
+        local a = Account1:create('bob_'..tostring(i), 1000)
+        table.insert(store, a)
+    end
+    local current_size = collectgarbage('count')
+    print('Account1 bytes: '..tostring((current_size - start_size) * 1024 / num, 10))
+    start_size = current_size
+
+    for i = 1, num do
+        local a = Account2:create('bob_'..tostring(i), 1000)
+        table.insert(store, a)
+    end
+    current_size = collectgarbage('count')
+    print('Account2 bytes: '..tostring((current_size - start_size) * 1024 / num, 10))
+    start_size = current_size
+
+    for i = 1, num do
+        local a = Account3('bob_'..tostring(i), 1000)
+        table.insert(store, a)
+    end
+    current_size = collectgarbage('count')
+    print('Account3 bytes: '..tostring((current_size - start_size) * 1024 / num, 10))
+    start_size = current_size
+
+    -- force garbage collection
+    store = {}
+    current_size = collectgarbage()
+
+    -- current_size = collectgarbage('count')
+    -- print('GC took', current_size - start_size, current_size)
+    -- start_size = current_size
+
+
 end
 
 
-globals:
-- basic: _G, _VERSION, assert, collectgarbage, dofile, error, getmetatable, ipairs, load, loadfile, next, pairs, pcall, print,
-    rawequal, rawget, rawlen, rawset, require, select, setmetatable, tonumber, tostring, type, warn, xpcall
-- modules: coroutine, debug, io, math, os, package, string, table, utf8, 
-- metamethods: __add, __band, __bnot, __bor, __bxor, __call, __close, __concat, __div, __eq, __gc, __idiv, __index, 
-    __le, __len, __lt, __metatable, __mod, __mode, __mul, __name, __newindex, __pairs, __pow, __shl, __shr, __sub,
-    __tostring, __unm
-
-Guarding against typos
+---------------------------------------------------------------------------
+--[[ Guarding against typos
 Indexing into a table in Lua gives you nil if the key isn't present, which can cause errors that are difficult to trace!
-Our other major use case for metatables is to prevent certain forms of this problem. For types that act like enums, we can carefully apply an __index metamethod that throws:
+Our other major use case for metatables is to prevent certain forms of this problem. For types that act like enums,
+we can carefully apply an __index metamethod that throws.
+Since __index is only called when a key is missing in the table, MyEnum.A and MyEnum.B will still give you back the expected values, but MyEnum.FROB will throw, hopefully helping engineers track down bugs more easily.
+]]
 local MyEnum = {
     A = "A",
     B = "B",
@@ -51,48 +185,11 @@ setmetatable(MyEnum, {
             tostring(key)), 2)
     end,
 })
-Since __index is only called when a key is missing in the table, MyEnum.A and MyEnum.B will still give you back the expected values, but MyEnum.FROB will throw, hopefully helping engineers track down bugs more easily.
 
 
- ? args can be
---   - utils.on_error 'quit'
---   - utils.on_error('quit')
---   - utils.on_error'quit'  ??
-local function myfunc( ... )
-    -- body
-end
+---------------------------------------------------------------------------
+--[[ switch/pattern matching 
 
-
-local function UT_RAISES(func, args, exp_msg)
-    local pass = true
-    -- M.num_cases_run = M.num_cases_run + 1
-
-    -- print(args, #args)
-
-    print(table.unpack(args))
-    -- local xx = args:unpack()
-
-    local ok, msg = pcall(func, table.unpack(args))
-
-    -- print('!!!', ok, msg) -- T
-
-    if ok then
-        print('fail: function did not raise error()')
-        pass = false
-    elseif sx.contains(msg, exp_msg) then
-        print('pass')
-        pass = true
-    else
-        print('fail: function did raise error() but ['..msg..'] does not contain ['..exp_msg..']')
-    end
-    return pass
-end
--- test
--- UT_RAISES(List, { 'muffin', 123, 'beetlejuice', 'tigger' }, '')
-
-
------------------------------------------------
-TODOF switch/pattern matching ----------------------
 -- https://stackoverflow.com/questions/37447704
 -- http://lua-users.org/wiki/SwitchStatement
 
@@ -128,115 +225,69 @@ local function switch_run(day, month, year, dtype)
 end
 ]]
 
----------------------------------- simple objects ------------------------------
-require('List')
 
--- >>>>>>>>>>>>>>>>>> https://www.lua.org/pil/16.1.html
--- >>>>>>>>>>>>>>>>>> https://www.reddit.com/r/lua/comments/tia21g/comment/i1dok2a/
--- http://lua-users.org/wiki/SimpleLuaClasses
+---------------------------------------------------------------------------
+--[[ organize modules/globals: https://www.lua.org/pil/15.4.html
 
-
-
--- 100000
--- start   81.423828125
--- Account1 took   28051.73046875  28133.154296875
--- Account2 took   43119.7578125   71252.912109375
--- GC took -70167.904296875    1085.0078125
-
--- 10000
--- start   81.408203125
--- Account1 took   2815.6630859375 2897.0712890625  >>> 288 bytes per
--- Account2 took   4290.6640625    7187.7353515625  >>> 439 bytes per
--- GC took -6998.7431640625    188.9921875
-
-
------------------------------------------------------------------------
--- Version using shared metatable and klunky '_P' table holding private fields. 288 bytes per.
-Account1 = { }
-Account1.__index = Account1
-Account1.__tostring = function(t) return string.format('[%s:%s:%d]', t:class(), t:name(), t.balance) end
-Account1.__newindex = function(t, index, value) rawset(t, index, value) end
--- Account1.__newindex = function(t, index, value) error('__newindex not supported') end
-
-function Account1:create(name, balance)
-    local o = {}
-
-    local p =
-    {
-        class='Account1',
-        name=name,
-        -- __index = Account1,
-        -- __metatable = Account1
-    }
-    o._P = p
-
-    -- ? getmetatable(object)
-    -- If object does not have a metatable, returns nil.
-    -- Else if the object's metatable has a __metatable field, returns the associated value.
-    -- Else returns the metatable of the given object. 
-
-    -- make Account1 handle lookup
-    setmetatable(o, Account1)
-
-    -- Real data.
-    o.balance = balance
-    return o
+require 'pl' -- calls Penlight\lua\pl\init.lua
+utils.import 'pl.func' -- take a table/module and 'inject' it into the local namespace.
+local ops = require 'pl.operator' -- normal import
+local List = require 'pl.List' -- class
+local append, concat = table.insert, table.concat -- aliases
+local optable = ops.optable -- alias
+--?? add init.lua file? see http://www.playwithlua.com/?p=64
+-- check the name before the assignment.
+function M.check_open_package(name) -->> check_global_name?
+    for n, v in pairs(name) do
+        if _G[n] ~= nil then
+            error("name clash: " .. n .. " is already defined")
+        end
+        _G[n] = v
+    end
 end
+]]
 
-function Account1:withdraw(amount)
-    self.balance = self.balance - amount
-end
 
-function Account1:getbalance()
-    return self.balance
-end
+---------------------------------------------------------------------------
+--[[ pnut helper for error() callers TODOL put in pnut and update tests.
+]]
 
-function Account1:name()
-    return self._P.name
-end
+local function case_failed(msg, info) print('FAIL:', msg, info) end
 
-function Account1:class()
-    return self._P.class
+local function UT_RAISES(func, args, exp_msg, info)
+    local pass = true
+    -- M.num_cases_run = M.num_cases_run + 1
+    local ok, msgcall = pcall(func, args)
+
+    if ok then
+        local msg = 'function did not raise error()'
+        case_failed(msg, info)
+        pass = false
+    elseif sx.contains(msgcall, exp_msg) then
+        pass = true
+    else
+        local msg = 'function did raise error() but ['..msgcall..'] does not contain ['..exp_msg..']'
+        case_failed(msg, info)
+        pass = false
+    end
+    return pass
 end
 
 
+---------------------------------------------------------------------------
 
------------------------------------------------------------------------
--- Version using individual metatables holding private fields. 439 bytes per.
-Account2 = { }
+-- What to do.
 
-function Account2:create(name, balance)
-    local o = {}
+-- test objects
+-- test_simple_objects()
 
-    local mt = 
-    {
-        class = 'Account2',
-        name = name,
-        __index = Account2,
-        __tostring = function(t) return string.format('[%s:%s:%d]', getmetatable(t).class, getmetatable(t).name, t.balance) end,
-        __newindex = function(t, index, value) rawset(t, index, value) end
-    }
-    setmetatable(o, mt)
 
-    -- Real data.
-    o.balance = balance
-    return o
+-- test UT_RAISES
+local function func_that_throws(args, exp_msg)
+    local a1, a2, a3, a4 = table.unpack(args)
+    if a1 == a2 + a3 + a4 then error("BOOM") end
 end
 
-function Account2:withdraw(amount)
-    self.balance = self.balance - amount
-end
-
-function Account2:getbalance()
-    return self.balance
-end
-
-function Account2:name()
-    return getmetatable(self).name
-end
-
-function Account2:class()
-    return getmetatable(self).class
-end
-
-
+local res = UT_RAISES(func_that_throws, {66, 1, 2, 3}, 'not_exp_msg')
+res = UT_RAISES(func_that_throws, {6, 1, 2, 3}, 'BOOM')
+res = UT_RAISES(func_that_throws, {6, 1, 2, 3}, 'exp_msg')
